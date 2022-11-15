c7866bcd8ee2e06241c06d85898b5a221c3fbd0e
Wetooa <103910243+Wetooa@users.noreply.github.com>
2022-11-15 12:57:30 +0800

    did some stuff but na lowbat nako so

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,13 +1,44 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider, ColorModeProvider, theme } from "@chakra-ui/react";
-import { createClient, Provider } from "urql";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import { LoginMutation, MeDocument, MeQuery } from "../generated/graphql";
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:5000/graphql/",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              result,
+              (result, query) => {
+                if (result.m)
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
